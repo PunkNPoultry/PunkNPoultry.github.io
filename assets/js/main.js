2ddb108f7619ec6e65c71affcf2baaf83513284f
Chris Mason <chris@chaione.com>
2016-11-11 14:05:10 -0600

    Javascript optimizations
    - added Google Analytics
    - combined JS files

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+---
+# Do not delete this line. This main JS file needs this front matter
+layout:
+---
+{% include_relative jquery-1.11.1.min.js %}
+{% include_relative jquery.fitvids.js %}
+{% include_relative index.js %}
+{% include_relative readingTime.min.js %}

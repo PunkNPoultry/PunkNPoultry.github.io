90a9d8b4436befdaabb8019139d1744a9212396d
Chris Mason <chris@chaione.com>
2016-11-14 14:44:51 -0600

    Inline CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,5 +4,5 @@ layout:
 ---
 {% include_relative jquery-1.11.1.min.js %}
 {% include_relative jquery.fitvids.js %}
-{% include_relative index.js %}
 {% include_relative readingTime.min.js %}
+{% include_relative index.js %}
